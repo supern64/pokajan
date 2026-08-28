4e13eaf9fdbb3d4b9973a0f70c23ace34022600e
SuperN64 <n64@cirnoslab.me>
2026-08-28 23:58:23 +0700

    wtf raylib had this the entire time??

--- src/scene/overlay_card_instructions.c
+++ src/scene/overlay_card_instructions.c
@@ -33,17 +33,13 @@ static void CardInstructionsRender(void *self) {
 	DrawRectangle(0, 0, 1920, 1080, (Color){ 0, 0, 0, 196 }); // bg
 	DrawRectangleRounded((Rectangle){ 100, 200, 1720, 680 }, 0.2, 30, DARKGREEN);
 
-	// we presume that the correct cards have already been loaded by GameInit
 	DrawFocusText("Preparing Your Deck:", (Vector2){ 170, 260 }, 70, WHITE);
 
-	char step1[140];
-	snprintf(
-		step1,
-		140,
-		"1. Take out the cards of %s, %s, %s, and %s.\n(each member has 3 colors of cards each, and 3 cards per color.)",
-		GENERATION_NAME[s->generations[0]], GENERATION_NAME[s->generations[1]], GENERATION_NAME[s->generations[2]], GENERATION_NAME[s->generations[3]]
+	DrawMainText(
+		TextFormat("1. Take out the cards of %s, %s, %s, and %s.\n(each member has 3 colors of cards each, and 3 cards per color.)",
+			GENERATION_NAME[s->generations[0]], GENERATION_NAME[s->generations[1]], GENERATION_NAME[s->generations[2]], GENERATION_NAME[s->generations[3]]), 
+		(Vector2){ 170, 360 }, 40, WHITE
 	);
-	DrawMainText(step1, (Vector2){ 170, 360 }, 40, WHITE);
 
 	DrawMainText("2. Shuffle the cards.", (Vector2){ 170, 490 }, 40, WHITE);
 
@@ -52,14 +48,10 @@ static void CardInstructionsRender(void *self) {
 		allGenMemSum += GENERATION_MEMBER_COUNT[s->generations[i]];
 	}
 
-	char step3[67];
-	snprintf(
-		step3,
-		67,
-		"3. After that, take out %d cards to make a 100-card deck.",
-		(allGenMemSum * 3 * 3) - 100
+	DrawMainText(
+		TextFormat("3. After that, take out %d cards to make a 100-card deck.", (allGenMemSum * 3 * 3) - 100), 
+		(Vector2){ 170, 555 }, 40, WHITE
 	);
-	DrawMainText(step3, (Vector2){ 170, 555 }, 40, WHITE);
 
 	DrawMainText("4. Then, deal 7 cards to each player.", (Vector2){ 170, 620 }, 40, WHITE);
 
