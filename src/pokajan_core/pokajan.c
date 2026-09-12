da5b97c2e060af1390275db319c49e3b38556be2
SuperN64 <n64@cirnoslab.me>
2026-09-12 16:46:36 +0700

    full game core

--- src/pokajan_core/pokajan.c
+++ src/pokajan_core/pokajan.c
@@ -11,6 +11,7 @@ void PokajanInit(Game *game) {
             game->players[i].hand[j] = EMPTY_CARD;
         }
         game->players[i].drawnSlot = EMPTY_CARD;
+        game->players[i].lastDiscard = EMPTY_CARD;
     }
 
     game->bonusCard = GetRandomBonusCard(game->generations);
@@ -24,4 +25,78 @@ void PokajanInit(Game *game) {
     game->discardClaimable = false;
     game->contestInProgress = false;
     game->contestants = 0;
+
+    game->ended = false;
+    game->endPending = false;
+}
+
+bool PokajanSetInitialHand(Game *game, int playerIndex, Card hand[7]) {
+    if (game->ended || game->endPending) return false;
+    // invalid if the players do not have all empty cards
+    for (int i = 0; i < 7; i++) {
+        if (!IS_CARD_EMPTY(game->players[playerIndex].hand[i])) return false;
+    }
+    for (int i = 0; i < 7; i++) {
+        game->players[playerIndex].hand[i] = hand[i];
+    }
+    game->cards -= 7;
+    return true;
+}
+
+bool PokajanDraw(Game *game, int playerIndex, Card card) {
+    if (game->ended || game->endPending) return false;
+    if (game->turnIndex != playerIndex) return false;
+    if (!IS_CARD_EMPTY(game->players[playerIndex].drawnSlot)) return false;
+    // ensure discard contest is closed
+    game->discardClaimable = false;
+    game->players[playerIndex].drawnSlot = card;
+    game->cards -= 1;
+    // bridge waits for discard
+    return true;
+}
+
+bool PokajanDiscardOnTurn(Game *game, int playerIndex, Card card) {
+    if (game->ended) return false;
+    if (game->turnIndex != playerIndex) return false; // it says it right there. discard ON TURN
+    int cardIndex = -1;
+    // make sure card exists
+    for (int i = 0; i < 7; i++) {
+        if (IS_SAME_CARD(game->players[playerIndex].hand[i], card)) {
+            cardIndex = i;
+            break;
+        }
+    }
+    // drawn slot
+    if (cardIndex == -1 && IS_SAME_CARD(game->players[playerIndex].drawnSlot, card)) cardIndex = 7;
+
+    if (cardIndex == -1) return false; // cannot discard, it doesn't exist
+
+    // move card from drawn slot into main slot if drawn slot isn't the one discarded
+    if (cardIndex != 7) {
+        game->players[playerIndex].lastDiscard = game->players[playerIndex].hand[playerIndex];
+        game->players[playerIndex].hand[playerIndex] = game->players[playerIndex].drawnSlot;
+        game->players[playerIndex].drawnSlot = EMPTY_CARD;
+    } else {
+        game->players[playerIndex].lastDiscard = game->players[playerIndex].drawnSlot;
+        game->players[playerIndex].drawnSlot = EMPTY_CARD;
+    }
+
+    // check ending flag set by PokajanReplenish/PokajanCommitSelfMatch/PokajanResolveContestAndCommitDiscardMatch
+    if (game->endPending || PokajanWillEnd(game)) {
+        game->ended = true;
+        // determine winners, game's over
+    } else {
+        game->discardClaimable = true;
+        game->turnIndex += 1;
+        if (game->turnIndex == 4) game->turnIndex = 0;
+    }
+}
+
+bool PokajanWillEnd(Game *game) {
+    if (game->cards == 0) return true;
+    // one player has 0 coins
+    for (int i = 0; i < 4; i++) {
+        if (game->players[i].coins == 0) return true;
+    }
+    return false;
 }
--- src/pokajan_core/pokajan.h
+++ src/pokajan_core/pokajan.h
@@ -5,14 +5,14 @@
 #include <stdbool.h>
 
 typedef enum {
-    THREE_OF_A_KIND, FULL_GENERATION
+    THREE_OF_A_KIND,
+    FULL_GENERATION
 } MatchPattern;
 
 typedef enum {
     SAME, // every single color is the same
     DIFFERENT // any color is different
 } MatchColor;
-
 
 typedef struct {
     int playerIndex; // equal to -1 if match does not exist
@@ -21,7 +21,7 @@ typedef struct {
     Card matchInHand[5]; // only counts cards in hand
     int reward;
     bool complete; // whether this match is available to claim
-    int useDiscardOf; // if the match involves a discard, this will be set to the player index whose discard was used.
+    int useDiscardOf; // player index of discard, -1 if all self
 } Match;
 
 typedef struct {
@@ -46,62 +46,29 @@ typedef struct {
     bool contestInProgress; // whether a contest is in progress
     Match contestMatch[3]; // the contested matches
     int contestants; // number of contestants
+
+    bool endPending;
+    bool ended;
 } Game;
 
-/*
-    (Game = main app loop, Engine = this)
-    Engine Loop:
-        - Initialize engine with PokajanInit, initialize generations and bonus and shows them to the players
-        - After the generations are shown, instructions are shown to create a 100-card deck (and this number is reflected in engine state)
-        - 7 cards are physically dealt out to each player.
-          Each card placed in a slot will call PokajanInsertDraw, and the engine should wait with WAIT_DRAW until all 28 draws are complete.
-        - Turn operation goes as follows:
-            - Player physically draws a card from the deck into the drawn slot, calls PokajanDraw
-            - Game checks eligible matches for the single player with PokajanCheckMatches
-                - If the player is eligible, the game will display options to Pokajan!
-                - If the player decides to Pokajan!, the game calls
-
-
-*/
-// Initializes the game data.
 void PokajanInit(Game *game);
-
 bool PokajanSetInitialHand(Game *game, int playerIndex, Card hand[7]);
 
-// Marks a card as drawn and puts it into the specified slot. Does not include turn draws into the hand slot.
-bool PokajanInsertDraw(Game *game, int playerIndex, Card card, int slot);
-
-// Registers a card draw into the drawn slot for a player. Closes the claiming period of the previous player's discard if one exists. Must be their turn if Pokajan! has not been claimed. Decreases internal card count. Returns success status.
 bool PokajanDraw(Game *game, int playerIndex, Card card);
-
-// Registers a discarded card for a player's turn. Must be their turn. Run when the player puts the card in the discard slot. Returns success status.
 bool PokajanDiscardOnTurn(Game *game, int playerIndex, Card card);
 
-// Registers discarded cards from a claimed match. Can only run if Pokajan! has been claimed. Runs whenever the player removes a card used for a match. Returns success status.
-bool PokajanDiscardAfterMatch(Game *game, int playerIndex, Card card);
-
-// Checks potential and possible matches for a player. Returns the number of matches.
 int PokajanCheckMatches(Game *game, int playerIndex, Match matches[20]);
-
-// Declares a contest on a discarded card. May only be done after a turn discard. Returns success status.
-bool PokajanDeclareContestOnDiscardMatch(Game *game, int playerIndex, Match match);
-
-/* 
-    Resolves the contest and commits the coin transfer.
-    Clears the contestant/contest in progress fields and waits for the winner to draw cards.
-    After winner has finished redrawing, check for a Pokajan! again with PokajanCheckMatches().
-    Returns false if no coin transfer happened due to no winner/nonexistent contest.
-*/
-bool PokajanResolveContestAndCommitDiscardMatch(Game *game, Match *outWinner);
-
-// Called after a match chain when no more matches are possible.
-// Clears matchInProgress/matchClaimant/drawsRemaining.
+bool PokajanCommitSelfMatch(Game *game, Match match);
+bool PokajanDiscardAfterMatch(Game *game, int playerIndex, Card card);
+bool PokajanReplenish(Game *game, int playerIndex, Card card, int slot);
 void PokajanEndMatchSequence(Game *game);
 
-// Registers a match from your own hands, commits the coin transfer, and waits for drawn cards. After player has finished redrawing, check for a Pokajan! again with PokajanCheckMatches(). Returns success status.
-bool PokajanCommitSelfMatch(Game *game, Match match);
+bool PokajanDeclareContestOnDiscardMatch(Game *game, int playerIndex, Match match);
+bool PokajanResolveContestAndCommitDiscardMatch(Game *game, Match *outWinner);
 
-// Returns true if the game will end due to a player having 0 coins or the deck running out of cards.
 bool PokajanWillEnd(Game *game);
+bool PokajanIsEnded(Game *game);
+void PokajanEnd(Game *game);
+int PokajanGetWinners(Game *game, int outWinners[4]);
 
 #endif
